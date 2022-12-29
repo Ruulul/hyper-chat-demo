8baecba49b3332f106d5a3d1c79ba2eba78893cb
Ruulul <vamjunior01@gmail.com>
2022-12-29 12:31:00 -0300

    minor changes

--- peer.mjs
+++ peer.mjs
@@ -19,14 +19,14 @@ function question (query = '') {
 
 const user = await question("Whats your user?\n:")
 const room_name = await question("In which room will you enter?\n:")
-const room = 'v142857-chat-demo'+room_name;
+const room = 'v142857-chat-demo-'+room_name;
 const topic = b4a.alloc(32).fill(room, 0, room.length)
 const swarm = new Hyperswarm()
 
 swarm.on('connection', conn => {
     int.write("Someone connected!\n")
     int.write(swarm.connections.size + " connections\n")
-    conn.on('data', data => int.write('\n' + data + ''))
+    conn.on('data', data => int.write('\r\n' + data + '\n'))
     conn.on('error', console.error)
 })
 
@@ -35,8 +35,8 @@ goodbye(async ()=>{
     int.write("Initializing shutdown sequence... ");
     await Promise.all(swarm.connections.map(conn=>conn.end()))
     await swarm.leave(topic)
-    await swarm.destroy();
-    int.write("done!\n");
+    await swarm.destroy()
+    int.write("done!\n")
 })
 
 await discovery.flushed().then(()=>int.write("Room joined succesfully\n"))
@@ -45,6 +45,6 @@ while (true) {
     const prefix = user + ': '
     const input = await question(prefix)
     swarm.connections.forEach(conn => {
-        conn.write(prefix + input);
+        conn.write(prefix + input)
     })
 }
