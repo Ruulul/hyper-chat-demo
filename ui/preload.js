0d85e5744bcb49981ffd145f54dd9e7f619b4922
Ruulul <vamjunior01@gmail.com>
2023-01-06 13:53:27 -0300

    start of code reuse

--- ui/preload.js
+++ ui/preload.js
@@ -1,8 +1,9 @@
 const { contextBridge, ipcRenderer } = require('electron')
+const { create_topic } = require('../peer.js')
 
 contextBridge.exposeInMainWorld('api', {
     b4a: require('b4a'),
-    join_topic: topic => ipcRenderer.invoke('join-topic', require('crypto').createHash('sha256').update(topic).digest()),
+    join_topic: topic => ipcRenderer.invoke('join-topic', create_topic(topic)),
     exit: () => ipcRenderer.send('exit'),
     send_message: message => ipcRenderer.invoke('send-message', message),
     onmessage: callback => ipcRenderer.on('new-message', callback),
