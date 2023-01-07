765f4f53b59a25e84bdb983803dc1aa63cf171ec
Ruulul <vamjunior01@gmail.com>
2023-01-07 14:27:50 -0300

    info protocol

--- README.md
+++ README.md
@@ -11,17 +11,19 @@ pnpm start # or pnpm run cli
 ## Using in code
 ```js
 const Chat = require("hyper-chat-demo")
-let chat_instance = await Chat(options = { swarm: swarm_instance, room: initial_room, nick: initial_nick }, 
+let chat_instance = await Chat(options = { swarm: swarm_instance, room: initial_room, info: initial_info }, 
     chat_instance => //protocol function, you can catch and manage the chat instance by the argument of this function instead
         message => { //listener function, receives message from the component
             const { head: [from], type, data } = message
             const handlers = { 
                 message, //your main event, sent by users
+                info, //your info/awareness info of the peer
+                update, //for partial updates of the info object
                 connection, //fired when a peer connects
                 disconnect, //fired when a peer disconnects
                 connections, //fired when the number of connections is notified (you request that event)
             }
             handlers[type]()
         })
-chat_instance({ type: "message" | "change-topic" | "connections" | "exit", data: /** the new topic, or the message you will send */ })
+chat_instance({ type: "message" | "change-topic" | "info" | "update" | "connections" | "exit", data: /** the new topic, info/info update, or the message you will send */ })
 ```
--- peer.js
+++ peer.js
@@ -21,45 +21,41 @@ function question(query = '') {
 async function run_cli() {
   const int = process.stdout
   var user = await question("Whats your user?\n:")
-  const prefix = ()=>user + ': '
+  const prefix = () => user + ': '
   const room_name = await question("In which room will you enter?\n:")
   const room = 'v142857-chat-demo-' + room_name;
   var chat_instance = undefined;
   const nicks = {}
-  await chat({ room, nick: user }, notify => {
+  await chat({ room, info: user }, notify => {
     chat_instance = notify
     return ({ head: [from] = [], type, data }) => {
       const handle = {
         connection, connections, disconnect,
-        message,
+        message, info, update: info,
       }
       handle[type]()
       function connection() {
-        int.write(`\n${from.toString('hex')} connected!\n${prefix()}`)
+        int.write(`\r${from.toString('hex')} connected!\n${prefix()}`)
         notify({ type: 'connections' })
       }
       function connections() {
-        int.write(`\n${data} connections\n`)
-        int.write(`\nknown nicks:\n`)
+        int.write(`\r${data} connections\n`)
+        int.write(`known nicks:\n`)
         for (const key in nicks) {
           int.write(`\t${nicks[key]} ( ${Buffer.from(key).toString('hex')} ),\n`)
         }
-        int.write(`\n${prefix()}`)
+        int.write(`${prefix()}`)
       }
       function disconnect() {
-        int.write(`\n${nicks[from] || from.toString('hex')} disconnected!\n${prefix()}`)
+        int.write(`\r${nicks[from] || from.toString('hex')} disconnected!\n${prefix()}`)
         delete nicks[from]
       }
+      function info() {
+        int.write(`\rpeer ${nicks[from] || from.toString('hex')} now is named ${data}\n${prefix()}`)
+        nicks[from] = data
+      }
       function message() {
-        const message_handles = { nick: handle_nick, message: handle_message }
-        for (const key of Object.keys(data)) message_handles[key]() 
-      }
-      function handle_nick() {
-        int.write(`\npeer ${nicks[from] || from.toString('hex')} now is named ${data.nick}\n${prefix()}`)
-        nicks[from] = data.nick
-      }
-      function handle_message() {
-        int.write(`\n${nicks[from] || "anom"}: ${data.message}\n${prefix()}`)
+        int.write(`\r${nicks[from] || "anom"}: ${data}\n${prefix()}`)
       }
     }
   })
@@ -67,28 +63,28 @@ async function run_cli() {
   while (true) {
     const input = await question(prefix())
     if (input === '/connections' || input === '/nicks') chat_instance({ type: 'connections' })
-    else if (input.startsWith('/nick')) 
-    { user = input.slice(input.indexOf(' ') + 1) 
-      chat_instance({ type: 'message', data: { nick: user }})
+    else if (input.startsWith('/nick')) {
+      user = input.slice(input.indexOf(' ') + 1)
+      chat_instance({ type: 'info', data: user })
     }
     else if (input.startsWith('/room')) chat_instance({ type: 'change-topic', data: input.slice(input.indexOf(' ') + 1) })
     else if (input in ['/exit', '/q', '/quit']) {
       await chat_instance({ type: 'exit' })
       break
     }
-    else chat_instance({ type: 'message', data: {message : input } })
+    else chat_instance({ type: 'message', data: input })
   }
 }
-async function chat({ swarm: swarm_instance, room: initial_room, nick: initial_nick } = {}, protocol) {
+async function chat({ swarm: swarm_instance, room: initial_room, info: initial_info } = {}, protocol) {
   const notify = protocol(listen)
   const swarm = swarm_instance || new Hyperswarm()
   goodbye(exit)
   const user_key = swarm.keyPair.publicKey
-  var nick = initial_nick
+  var info = initial_info
   swarm.on('connection', (conn, { publicKey: peer }) => {
-    if (nick) conn.write(JSON.stringify({ head: [user_key], type: 'message', data: { nick } }))
+    if (info) conn.write(JSON.stringify({ head: [user_key], type: 'info', data: info }))
     notify({ head: [peer], type: 'connection' })
-    conn.on('data', data => notify({...JSON.parse(data), head: [peer]}))
+    conn.on('data', data => notify({ ...JSON.parse(data), head: [peer] }))
     conn.on('close', () => notify({ type: 'disconnect', head: [peer] }))
     conn.on('error', e => (console.error(e), notify({ type: 'disconnect', head: [peer] })))
   })
@@ -98,7 +94,9 @@ async function chat({ swarm: swarm_instance, room: initial_room, nick: initial_n
   async function listen(message) {
     const { head: [from] = [], type, data } = message
     const handle = {
-      "message": send_message,
+      message: send_message,
+      info: send_message,
+      update: send_message,
       "change-topic": change_topic,
       "connections": notify_connections,
       exit,
