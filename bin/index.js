99cc46d18a454b8d70cafe7b42e24505c2322da4
Ruulul <vamjunior01@gmail.com>
2023-01-11 19:39:53 -0300

    improved TUI!

--- bin/index.js
+++ bin/index.js
@@ -2,17 +2,19 @@
 
 const readline = require('readline')
 const chat = require('..')
-const int = process.stdout
+let buffer = ''
+const fill_buffer = key => buffer += key
 
 cli()
 
 async function cli() {
-    var user = await question("Whats your user?\n:")
-    const prefix = () => user + ': '
+    const int = process.stdout
+    let user = await question("Whats your user?\n:")
+    const prefix = () => user + ': ' + buffer
     const room_name = await question("In which room will you enter?\n:")
     const room = 'v142857-chat-demo-' + room_name;
     const nicks = {}
-    const chat_instance = await chat({ room, info: user }, notify => {
+    const chat_instance = await chat({ room, info: user }, _ => {
         return ({ head: [from] = [], type, data }) => {
             const handles = {
                 connections, disconnect,
@@ -40,12 +42,16 @@ async function cli() {
                 nicks[from] = data
             }
             function message() {
-                int.write(`\r${nicks[from] || "anom"}: ${data}\n${prefix()}`)
+                int.write(`\r${nicks[from] || "anom"}: ${data}${
+                    ' '.repeat(process.stdout.getWindowSize()[0] - data.length - (nicks[from] || "anom").length - 2)
+                }\n${prefix()}`)
             }
         }
     })
     int.write("\nEntered successfully!\n")
     const commands = {
+        help: display_this_help,
+        '?': display_this_help,
         connections: require_connections,
         nicks: require_connections,
         nick: change_nick,
@@ -59,28 +65,39 @@ async function cli() {
         const space_index = input.indexOf(' ')
         const after_space = space_index > 0 ? input.slice(space_index + 1) : undefined
         const command = input.slice(1, space_index > 0 ? space_index : undefined)
-        if (input[0] === '/' && command in commands) await commands[command](after_space)
-        else chat_instance({ type: 'message', data: input })
+        if (input[0] === '/') {
+            if (command in commands) await commands[command](after_space)
+            else int.write(`\nCommand not recognized! (type /? or /help to see the commands)\n${prefix()}`)
+        } else chat_instance({ type: 'message', data: input })
 
         if (['exit', 'q', 'quit'].includes(command)) break
     }
-
-    function require_connections () {
+    function display_this_help() {
+        help()
+    }
+    function help() {
+        for (const key in commands) {
+            int.write(`${key}: ${commands[key].name.replace(/_/g, ' ')}\n`)
+        }
+        int.write('\n')
+    }
+    function require_connections() {
         return chat_instance({ type: 'connections' })
     }
-    function change_nick (data) {
+    function change_nick(data) {
         user = data
         return chat_instance({ type: 'update', data })
     }
-    function change_room (data) {
+    function change_room(data) {
         return chat_instance({ type: 'change-topic', data })
     }
-    function exit () {
+    function exit() {
         return chat_instance({ type: 'exit' })
     }
 }
 
 function question(query = '') {
+    if (!process.stdin.listeners('data').includes(fill_buffer)) process.stdin.on('data', fill_buffer)
     return new Promise(resolve => {
         const rl = readline.createInterface({
             input: process.stdin,
@@ -90,6 +107,7 @@ function question(query = '') {
         rl.question(query, function (answer) {
             rl.close()
             resolve(answer)
+            buffer = ''
         })
     })
 }
